dc4404ec1aa5e2c134c74329c960304dfe278379
joao <joao182cx@gmail.com>
2019-01-28 19:01:00 -0200

    Mudança de Polygon para GeneralPath.

--- src/Triangulos.java
+++ src/Triangulos.java
@@ -1,7 +1,7 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Polygon;
+import java.awt.geom.GeneralPath;
 import java.security.SecureRandom;
 
 import javax.swing.JPanel;
@@ -13,20 +13,24 @@ public class Triangulos extends JPanel {
 	@Override
 	public void paintComponent(Graphics g) {
 		
+		super.paintComponent(g);
+		
 		Graphics2D g2d = (Graphics2D) g;
-		Polygon poligono = new Polygon();
+		GeneralPath triangulo = new GeneralPath();
 		
 		
 		
-		for(int i = 0; i < aleatorio.nextInt(10); i++) {
+		for(int i = 0; i < aleatorio.nextInt(100); i++) {
 			
 			g2d.setColor(new Color(aleatorio.nextInt(256), aleatorio.nextInt(256), aleatorio.nextInt(256)));
 			
-			poligono.addPoint(aleatorio.nextInt(getWidth()), aleatorio.nextInt(getHeight()));
-			poligono.addPoint(aleatorio.nextInt(getWidth()), aleatorio.nextInt(getHeight()));
-			poligono.addPoint(aleatorio.nextInt(getWidth()), aleatorio.nextInt(getHeight()));
+			triangulo.moveTo(aleatorio.nextInt(getWidth()), aleatorio.nextInt(getHeight()));
+			triangulo.lineTo(aleatorio.nextInt(getWidth()), aleatorio.nextInt(getHeight()));
+			triangulo.lineTo(aleatorio.nextInt(getWidth()), aleatorio.nextInt(getHeight()));
 			
-			g2d.fillPolygon(poligono);
+			triangulo.closePath();
+			
+			g2d.fill(triangulo);
 			
 		}
 		
