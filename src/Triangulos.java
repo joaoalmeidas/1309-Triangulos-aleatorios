96bbd5a2d264044d044558d5a4b0b135e508b2b1
joao <joao182cx@gmail.com>
2019-01-28 22:48:06 -0200

    Correção de erro que estava atrapalhando a visualização dos triangulos gerados aleatoriamente.

--- src/Triangulos.java
+++ src/Triangulos.java
@@ -3,33 +3,34 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.GeneralPath;
 import java.security.SecureRandom;
+import java.util.Scanner;
 
 import javax.swing.JPanel;
 
 public class Triangulos extends JPanel {
 
-	final SecureRandom aleatorio = new SecureRandom();
+	
 	
 	@Override
 	public void paintComponent(Graphics g) {
 		
 		super.paintComponent(g);
+		SecureRandom aleatorio = new SecureRandom();
 		
 		Graphics2D g2d = (Graphics2D) g;
-		GeneralPath triangulo = new GeneralPath();
 		
 		
 		
-		for(int i = 0; i < aleatorio.nextInt(100); i++) {
+		for(int i = 0; i < aleatorio.nextInt(100000); i++) {
 			
-			g2d.setColor(new Color(aleatorio.nextInt(256), aleatorio.nextInt(256), aleatorio.nextInt(256)));
+			GeneralPath triangulo = new GeneralPath();
 			
 			triangulo.moveTo(aleatorio.nextInt(getWidth()), aleatorio.nextInt(getHeight()));
 			triangulo.lineTo(aleatorio.nextInt(getWidth()), aleatorio.nextInt(getHeight()));
 			triangulo.lineTo(aleatorio.nextInt(getWidth()), aleatorio.nextInt(getHeight()));
 			
 			triangulo.closePath();
-			
+			g2d.setColor(new Color(aleatorio.nextFloat(), aleatorio.nextFloat(), aleatorio.nextFloat(), 1));
 			g2d.fill(triangulo);
 			
 		}
